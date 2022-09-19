959d4233380bee26276e2fdc31af14805cf1c12f
Gorka Benguria <gorka.benguria@tecnalia.com>
2022-09-19 11:27:49 +0200

    just one machine

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,9 +26,9 @@ resource "openstack_compute_instance_v2" "OracleDB" {
   name        = "concrete_oracle_db"
   image_name  = "Ubuntu-Focal-20.04-Daily-2022-04-19"
   flavor_name = "small"
-  key_pair    = openstack_compute_keypair_v2.DbKeyName.name
+  key_pair    = openstack_compute_keypair_v2.user1.name
   network { 
-    port = openstack_networking_port_v2.vpc.id
+    port = openstack_networking_port_v2.net1_subnet.id
     
   }
 }
@@ -47,132 +47,120 @@ resource "openstack_compute_floatingip_associate_v2" "OracleDB_floating_ip_assoc
 
 
 
-# Create virtual machine
-resource "openstack_compute_instance_v2" "gestaut_vm" {
-  name        = "concrete_gestaut_vm"
-  image_name  = "Ubuntu-Focal-20.04-Daily-2022-04-19"
-  flavor_name = "small"
-  key_pair    = openstack_compute_keypair_v2.GestautKeyName.name
-  network { 
-    port = openstack_networking_port_v2.vpc.id
+# # Create virtual machine
+# resource "openstack_compute_instance_v2" "gestaut_vm" {
+#   name        = "concrete_gestaut_vm"
+#   image_name  = "Ubuntu-Focal-20.04-Daily-2022-04-19"
+#   flavor_name = "small"
+#   key_pair    = openstack_compute_keypair_v2.user1.name
+#   network { 
+#     port = openstack_networking_port_v2.net1_subnet.id
     
-  }
-}
+#   }
+# }
 
-# Create floating ip
-resource "openstack_networking_floatingip_v2" "gestaut_vm_floating_ip" {
-  pool = "external"
-  # fixed_ip = ""
-}
+# # Create floating ip
+# resource "openstack_networking_floatingip_v2" "gestaut_vm_floating_ip" {
+#   pool = "external"
+#   # fixed_ip = ""
+# }
 
-# Attach floating ip to instance
-resource "openstack_compute_floatingip_associate_v2" "gestaut_vm_floating_ip_association" {
-  floating_ip = openstack_networking_floatingip_v2.gestaut_vm_floating_ip.address
-  instance_id = openstack_compute_instance_v2.gestaut_vm.id
-}
+# # Attach floating ip to instance
+# resource "openstack_compute_floatingip_associate_v2" "gestaut_vm_floating_ip_association" {
+#   floating_ip = openstack_networking_floatingip_v2.gestaut_vm_floating_ip.address
+#   instance_id = openstack_compute_instance_v2.gestaut_vm.id
+# }
 
 
 
-# Create virtual machine
-resource "openstack_compute_instance_v2" "elasticsearch_vm" {
-  name        = "elasticsearch_vm"
-  image_name  = "Ubuntu-Focal-20.04-Daily-2022-04-19"
-  flavor_name = "small"
-  key_pair    = openstack_compute_keypair_v2.ESKeyName.name
-  network { 
-    port = openstack_networking_port_v2.vpc.id
+# # Create virtual machine
+# resource "openstack_compute_instance_v2" "elasticsearch_vm" {
+#   name        = "elasticsearch_vm"
+#   image_name  = "Ubuntu-Focal-20.04-Daily-2022-04-19"
+#   flavor_name = "small"
+#   key_pair    = openstack_compute_keypair_v2.user1.name
+#   network { 
+#     port = openstack_networking_port_v2.net1_subnet.id
     
-  }
-}
+#   }
+# }
 
-# Create floating ip
-resource "openstack_networking_floatingip_v2" "elasticsearch_vm_floating_ip" {
-  pool = "external"
-  # fixed_ip = ""
-}
+# # Create floating ip
+# resource "openstack_networking_floatingip_v2" "elasticsearch_vm_floating_ip" {
+#   pool = "external"
+#   # fixed_ip = ""
+# }
 
-# Attach floating ip to instance
-resource "openstack_compute_floatingip_associate_v2" "elasticsearch_vm_floating_ip_association" {
-  floating_ip = openstack_networking_floatingip_v2.elasticsearch_vm_floating_ip.address
-  instance_id = openstack_compute_instance_v2.elasticsearch_vm.id
-}
+# # Attach floating ip to instance
+# resource "openstack_compute_floatingip_associate_v2" "elasticsearch_vm_floating_ip_association" {
+#   floating_ip = openstack_networking_floatingip_v2.elasticsearch_vm_floating_ip.address
+#   instance_id = openstack_compute_instance_v2.elasticsearch_vm.id
+# }
 
 
 
-# Create virtual machine
-resource "openstack_compute_instance_v2" "edi_vm" {
-  name        = "edi_vm"
-  image_name  = "Ubuntu-Focal-20.04-Daily-2022-04-19"
-  flavor_name = "small"
-  key_pair    = openstack_compute_keypair_v2.EdiKeyName.name
-  network { 
-    port = openstack_networking_port_v2.vpc.id
+# # Create virtual machine
+# resource "openstack_compute_instance_v2" "edi_vm" {
+#   name        = "edi_vm"
+#   image_name  = "Ubuntu-Focal-20.04-Daily-2022-04-19"
+#   flavor_name = "small"
+#   key_pair    = openstack_compute_keypair_v2.user1.name
+#   network { 
+#     port = openstack_networking_port_v2.net1_subnet.id
     
-  }
-}
+#   }
+# }
 
-# Create floating ip
-resource "openstack_networking_floatingip_v2" "edi_vm_floating_ip" {
-  pool = "external"
-  # fixed_ip = ""
-}
+# # Create floating ip
+# resource "openstack_networking_floatingip_v2" "edi_vm_floating_ip" {
+#   pool = "external"
+#   # fixed_ip = ""
+# }
 
-# Attach floating ip to instance
-resource "openstack_compute_floatingip_associate_v2" "edi_vm_floating_ip_association" {
-  floating_ip = openstack_networking_floatingip_v2.edi_vm_floating_ip.address
-  instance_id = openstack_compute_instance_v2.edi_vm.id
-}
+# # Attach floating ip to instance
+# resource "openstack_compute_floatingip_associate_v2" "edi_vm_floating_ip_association" {
+#   floating_ip = openstack_networking_floatingip_v2.edi_vm_floating_ip.address
+#   instance_id = openstack_compute_instance_v2.edi_vm.id
+# }
 
 
 ## Network
 
 # Create Network
-resource "openstack_networking_network_v2" "vpc" {
-  name = "concrete_vpc"
+resource "openstack_networking_network_v2" "net1" {
+  name = "concrete_net"
 }
 
-# Subnet
-resource "openstack_networking_subnet_v2" "subnet1_subnet" {
-  name            = "subnet1_subnet"
-  network_id      = openstack_networking_network_v2.vpc.id
-  cidr            = "10.100.1.0/24"
-  dns_nameservers = ["8.8.8.8", "8.8.8.4"]
-}# Subnet
-resource "openstack_networking_subnet_v2" "subnet2_subnet" {
-  name            = "subnet2_subnet"
-  network_id      = openstack_networking_network_v2.vpc.id
-  cidr            = "10.100.2.0/24"
-  dns_nameservers = ["8.8.8.8", "8.8.8.4"]
-}# Subnet
-resource "openstack_networking_subnet_v2" "subnet3_subnet" {
-  name            = "subnet3_subnet"
-  network_id      = openstack_networking_network_v2.vpc.id
-  cidr            = "10.100.3.0/24"
+# Create Subnet
+resource "openstack_networking_subnet_v2" "net1_subnet" {
+  name            = "concrete_net_subnet"
+  network_id      = openstack_networking_network_v2.net1.id
+  cidr            = "16.0.0.0/24"
   dns_nameservers = ["8.8.8.8", "8.8.8.4"]
 }
 
 # Attach networking port
-resource "openstack_networking_port_v2" "vpc" {
-  name           = "concrete_vpc"
-  network_id     = openstack_networking_network_v2.vpc.id
+resource "openstack_networking_port_v2" "net1" {
+  name           = "concrete_net"
+  network_id     = openstack_networking_network_v2.net1.id
   admin_state_up = true
   security_group_ids = [
-  
+  openstack_compute_secgroup_v2.nginx.id,
   ]
-  fixed_ip { ## TODO to be fixed (not working for posidonia example, needed for openstack example)
-    subnet_id = openstack_networking_subnet_v2.subnet1_subnet.id
+  fixed_ip {
+    subnet_id = openstack_networking_subnet_v2.net1_subnet.id
   }
 }
 
 # Create router
-resource "openstack_networking_router_v2" "vpc_router" {
-  name                = "vpc_router"
+resource "openstack_networking_router_v2" "net1_router" {
+  name                = "net1_router"
   external_network_id = data.openstack_networking_network_v2.external.id    #External network id
 }
 # Router interface configuration
-resource "openstack_networking_router_interface_v2" "vpc_router_interface" {
-  router_id = openstack_networking_router_v2.vpc_router.id
-  subnet_id = openstack_networking_subnet_v2.subnet1_subnet.id ## TODO to be fixed (not working for posidonia example, needed for openstack example)
+resource "openstack_networking_router_interface_v2" "net1_router_interface" {
+  router_id = openstack_networking_router_v2.net1_router.id
+  subnet_id = openstack_networking_subnet_v2.net1_subnet.id
 }
 
 # generate random string
@@ -184,32 +172,38 @@ resource "random_string" "key_pair_user_name" {
 }
 
 # Create ssh keys
-resource "openstack_compute_keypair_v2" "DbKeyName" {
+resource "openstack_compute_keypair_v2" "user1" {
   name       = random_string.key_pair_user_name.result
   # public_key = "oracledb"
 }
 
-
-
-# Create ssh keys
-resource "openstack_compute_keypair_v2" "GestautKeyName" {
-  name       = "gestaut"
-  # public_key = "gestaut"
+# CREATING SECURITY_GROUP
+  
+resource "openstack_compute_secgroup_v2" "nginx" {
+  name        = "nginx"
+  description  = "Security group rule for nginx"
+  rule {
+    from_port   = -1
+    to_port     = -1
+    ip_protocol = "icmp"
+    cidr        = "0.0.0.0/0"
+  }
+  rule {
+    from_port   = 80
+    to_port     = 80
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+  rule {
+    from_port   = 443
+    to_port     = 443
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
 }
-
-
-
-# Create ssh keys
-resource "openstack_compute_keypair_v2" "ESKeyName" {
-  name       = "es"
-  # public_key = "es"
-}
-
-
-
-# Create ssh keys
-resource "openstack_compute_keypair_v2" "EdiKeyName" {
-  name       = "edi"
-  # public_key = "edi"
-}
-
