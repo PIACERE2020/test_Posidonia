521bf812f8700061f7e613d23739c7d6ff46deec
Gorka Benguria <gorka.benguria@tecnalia.com>
2022-09-19 11:54:33 +0200

    adjust outputs

--- terraform/output.tf
+++ terraform/output.tf
@@ -12,60 +12,15 @@ output "instance_ip_OracleDB" {
   value = openstack_compute_floatingip_associate_v2.OracleDB_floating_ip_association.floating_ip
 }
 
+output "instance_ip_gestaut_vm" {
+  value = openstack_compute_floatingip_associate_v2.gestaut_vm_floating_ip_association.floating_ip
+}
 
+output "instance_ip_elasticsearch_vm" {
+  value = openstack_compute_floatingip_associate_v2.elasticsearch_vm_floating_ip_association.floating_ip
+}
 
-
-# output "instance_server_public_key_DbKeyName" {
-#   value = openstack_compute_keypair_v2.DbKeyName.public_key
-# }
-
-# output "instance_server_private_key_DbKeyName" {
-#   value = openstack_compute_keypair_v2.DbKeyName.private_key
-# }
-
-# output "instance_ip_OracleDB" {
-#   value = openstack_compute_floatingip_associate_v2.OracleDB_floating_ip_association.floating_ip
-# }
-
-
-
-# output "instance_server_public_key_GestautKeyName" {
-#   value = openstack_compute_keypair_v2.GestautKeyName.public_key
-# }
-
-# output "instance_server_private_key_GestautKeyName" {
-#   value = openstack_compute_keypair_v2.GestautKeyName.private_key
-# }
-
-# output "instance_ip_gestaut_vm" {
-#   value = openstack_compute_floatingip_associate_v2.gestaut_vm_floating_ip_association.floating_ip
-# }
-
-
-
-# output "instance_server_public_key_ESKeyName" {
-#   value = openstack_compute_keypair_v2.ESKeyName.public_key
-# }
-
-# output "instance_server_private_key_ESKeyName" {
-#   value = openstack_compute_keypair_v2.ESKeyName.private_key
-# }
-
-# output "instance_ip_elasticsearch_vm" {
-#   value = openstack_compute_floatingip_associate_v2.elasticsearch_vm_floating_ip_association.floating_ip
-# }
-
-
-
-# output "instance_server_public_key_EdiKeyName" {
-#   value = openstack_compute_keypair_v2.EdiKeyName.public_key
-# }
-
-# output "instance_server_private_key_EdiKeyName" {
-#   value = openstack_compute_keypair_v2.EdiKeyName.private_key
-# }
-
-# output "instance_ip_edi_vm" {
-#   value = openstack_compute_floatingip_associate_v2.edi_vm_floating_ip_association.floating_ip
-# }
+output "instance_ip_edi_vm" {
+  value = openstack_compute_floatingip_associate_v2.edi_vm_floating_ip_association.floating_ip
+}
 
